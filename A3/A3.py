023536a4cab1bb158cbcc72784bd68106de0fe2c
Andreas Pedersen <pedersenandreas45@gmail.com>
2025-08-26 16:44:17 +0200

    Added A3 init

---
+++ A3/A3.py
@@ -0,0 +1,123 @@
+from numpy import ndarray
+
+
+def load_data(path: str) -> tuple[list[str], list[int]]:
+    """Loads data from file. Each except first (header) is a datapoint
+    containing ID, Label, Email (content) separated by "\t". Lables should be
+    changed into integers with 1 for "spam" and 0 for "ham".
+
+    Args:
+        path: Path to file from which to load data
+
+    Returns:
+        List of email contents and a list of lobels coresponding to each email.
+    """
+    # TODO
+    return [], []
+
+
+def preprocess(doc: str) -> str:
+    """Preprocesses text to prepare it for feature extraction.
+
+    Args:
+        doc: String comprising the unprocessed contents of some email file.
+
+    Returns:
+        String comprising the corresponding preprocessed text.
+    """
+    # TODO
+    ...
+
+
+def preprocess_multiple(docs: list[str]) -> list[str]:
+    """Preprocesses multiple texts to prepare them for feature extraction.
+
+    Args:
+        docs: List of strings, each consisting of the unprocessed contents
+            of some email file.
+
+    Returns:
+        List of strings, each comprising the corresponding preprocessed
+            text.
+    """
+    # TODO
+    return []
+
+
+def extract_features(
+    train_dataset: list[str], test_dataset: list[str]
+) -> tuple[ndarray, ndarray] | tuple[list[float], list[float]]:
+    """Extracts feature vectors from a preprocessed train and test datasets.
+
+    Args:
+        train_dataset: List of strings, each consisting of the preprocessed
+            email content.
+        test_dataset: List of strings, each consisting of the preprocessed
+            email content.
+
+    Returns:
+        A tuple of of two lists. The lists contain extracted features for
+          training and testing dataset respectively.
+    """
+    # TODO
+    ...
+
+
+def train(X: ndarray, y: list[int]) -> object:
+    """Trains a classifier on extracted feature vectors.
+
+    Args:
+        X: Numerical array-like object (2D) representing the instances.
+        y: Numerical array-like object (1D) representing the labels.
+
+    Returns:
+        A trained model object capable of predicting over unseen sets of
+            instances.
+    """
+    # TODO
+    return None
+
+
+def evaluate(y: list[int], y_pred: list[int]) -> tuple[float, float, float, float]:
+    """Evaluates a model's predictive performance with respect to a labeled
+    dataset.
+
+    Args:
+        y: Numerical array-like object (1D) representing the true labels.
+        y_pred: Numerical array-like object (1D) representing the predicted
+            labels.
+
+    Returns:
+        A tuple of four values: recall, precision, F_1, and accuracy.
+    """
+    # TODO
+    return 0, 0, 0, 0
+
+
+if __name__ == "__main__":
+    print("Loading data...")
+    train_data_raw, train_labels = load_data("data/train.tsv")
+    test_data_raw, test_labels = load_data("data/test.tsv")
+
+    print("Processing data...")
+    train_data = preprocess_multiple(train_data_raw)
+    test_data = preprocess_multiple(test_data_raw)
+
+    print("Extracting features...")
+    train_feature_vectors, test_feature_vectors = extract_features(
+        train_data, test_data
+    )
+
+    print("Training...")
+    classifier = train(train_feature_vectors, train_labels)
+
+    print("Applying model on test data...")
+    predicted_labels = classifier.predict(test_feature_vectors)
+
+    print("Evaluating...")
+    recall, precision, f1, accuracy = evaluate(test_labels, predicted_labels)
+
+    print(f"Recall:\t{recall}")
+    print(f"Precision:\t{precision}")
+    print(f"F1:\t{f1}")
+    print(f"Accuracy:\t{accuracy}")
